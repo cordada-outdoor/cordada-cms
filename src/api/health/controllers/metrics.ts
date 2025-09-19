aa139171a1722ccfd5e6e46c71abb63e0851fb9b
Marc Marina <marc.marina.miravitlles@gmail.com>
2025-09-19 13:00:32 +0100

    Removed axios and switched node-fetch

--- src/api/health/controllers/metrics.ts
+++ src/api/health/controllers/metrics.ts
@@ -1,10 +1,8 @@
-import axios from "axios";
-
 export default {
   async index(ctx) {
-    const res = await axios.get("http://0.0.0.0:9000/metrics");
+    const res = await fetch("http://0.0.0.0:9000/metrics");
 
     ctx.status = 200;
-    ctx.body = res.data;
+    ctx.body = await res.text();
   },
 };
