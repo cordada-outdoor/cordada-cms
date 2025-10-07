e79a31aad9ae6c4d48f9809d9c0123c3c54632c2
Marc Marina <marc.marina.miravitlles@gmail.com>
2025-10-07 22:25:05 +0200

    Updated buckets

--- src/middlewares/metrics-middleware.ts
+++ src/middlewares/metrics-middleware.ts
@@ -3,6 +3,7 @@ import promBundle from "express-prom-bundle";
 export default (config, { strapi }) => {
   // Configure express-prom-bundle
   const metricsMiddleware = promBundle({
+    buckets: [0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1, 2.5, 5, 10, 15],
     includeMethod: true,
     includePath: true,
     includeStatusCode: true,
